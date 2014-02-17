4b4c08b08e707b2e690f53ef3f590a7dc8a6c078
Ankita kanitkar <Ankita_Kanitkar@external.mckinsey.com>
2014-02-17 19:16:33 +0530

    updated specs

--- spec/array_zipper_spec.rb
+++ spec/array_zipper_spec.rb
@@ -13,5 +13,14 @@ describe ArrayZipper do
  	zip_array.should == []
  	zip_array.size.should == 0
  end
- 
+ it "should return array of same size as the non-nil string tokens when one of the string is nil" do
+ 	zip_array = ArrayZipper.zipper(TWO_TOKEN_STRING1 ,nil) 
+ 	zip_array.should == [['first name',nil],['last name', nil]]
+ 	zip_array.size.should == 2
+ end
+ it "should return array of the size of the bigger string(which has more tokens)" do
+ 	zip_array = ArrayZipper.zipper(TWO_TOKEN_STRING1 ,"Ankita") 
+ 	zip_array.should == [['first name','Ankita'],['last name', nil]]
+ 	zip_array.size.should == 2
+ end
 end
